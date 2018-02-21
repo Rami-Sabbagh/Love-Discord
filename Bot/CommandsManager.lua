bdf5a58b0f6c8eb265b9ca7ca7b5a9263a2c621e
RamiLego4Game <ramilego4game@gmail.com>
2018-02-21 20:48:14 +0200

    Fix reload

--- Bot/CommandsManager.lua
+++ Bot/CommandsManager.lua
@@ -1,22 +1,35 @@
 --Love-Discord Bot Commands Manager
 
+local discord = require("Discord")
+
 local cm = {}
 
 cm.commands = {} --The loaded commands chunks
-cm.prefix = "." --The default prefix is '.'
+cm.prefixes = {}
+
+if love.filesystem.exists("prefix.json") then
+  cm.prefixes = discord.json:decode(love.filesystem.read("prefix.json"))
+else
+  love.filesystem.write("prefix.json",discord.json:encode_pretty(cm.prefixes))
+end
 
 function cm.reload()
   cm.commands = {} --Clear the commands table
   
   for id, name in ipairs(love.filesystem.getDirectoryItems("Bot/Commands")) do
-    local chunk, err = love.filesystem.load("Bot/Commands/"..name)
-    if chunk then
+    local ok, chunk = pcall(love.filesystem.load,"Bot/Commands/"..name)
+    if ok then
       cm.commands[name:sub(1,-5)] = chunk
       print("Loaded",name:sub(1,-5))
     else
-      print("Failed to load command '"..name.."'",err)
+      print("Failed to load command '"..name.."'",chunk)
     end
   end
+end
+
+function cm.setPrefix(p)
+  cm.prefix = p
+  love.filesystem.write("prefix.txt",cm.prefix)
 end
 
 return cm
