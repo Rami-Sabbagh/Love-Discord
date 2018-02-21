e3bb1d33f47c4134ee8db9d2dc3453c8ee037f4c
RamiLego4Game <ramilego4game@gmail.com>
2018-02-21 11:39:04 +0200

    Bugfix Discord libraries loading

--- Discord/init.lua
+++ Discord/init.lua
@@ -1,25 +1,30 @@
 --LIKO-12 Discord Library
 
 local libpath = ...
+local libdir = libpath:gsub("%.","/").."/"
 
 local discord = {}
+
+discord.requireLib = function(name)
+  return assert(love.filesystem.load(libdir..name..".lua"))(discord)
+end
 
 --ThirdParty-Libraries
 discord.json = require(libpath..".JSON")
 
 --Constants
 discord.apiEndpoint = "https://discordapp.com/api/v6/"
-discord.httpcodes = require(libpath..".httpcodes", discord)
+discord.httpcodes = discord.requireLib("httpcodes")
 
 --The library path
 discord.path = libpath
 
 --JSON HTTP Request
-discord.request = require(libpath..".request", discord)
-discord.tools = require(libpath..".tools", discord)
+discord.request = discord.requireLib("request")
+discord.tools = discord.requireLib("tools")
 
 --The configuration
-discord.config = require(libpath..".config", discord)
+discord.config = discord.requireLib("config")
 
 --Bot authorization
 if discord.config.bot_token then
@@ -27,9 +32,9 @@ if discord.config.bot_token then
 end
 
 --The modules
-discord.oauth = require(libpath..".oauth", discord)
-discord.gateway = require(libpath..".gateway", discord)
-discord.users = require(libpath..".users", discord)
-discord.channels = require(libpath..".channels", discord)
+discord.oauth = discord.requireLib("oauth")
+discord.gateway = discord.requireLib("gateway")
+discord.users = discord.requireLib("users")
+discord.channels = discord.requireLib("channels")
 
 return discord
