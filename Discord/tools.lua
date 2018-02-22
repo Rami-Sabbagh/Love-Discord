7b23a8c1e3fa42402322e1d8464caedecdf76cf7
RamiLego4Game <ramilego4game@gmail.com>
2018-02-22 22:04:32 +0200

    Added premissions decoder

--- Discord/tools.lua
+++ Discord/tools.lua
@@ -38,6 +38,21 @@ function tools.generateSnowflake()
   return tools.time2Snowflake(os.time())
 end
 
+local pnames = {
+  "CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "ADMINISTRATOR", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS",
+}
+
+function tools.decodePermissions(perm)
+  local p = {}
+  for k, permission in ipairs(pnames) do
+    if bit.band(perm,1) > 0 then
+      p[permission] = true
+    end
+    perm = bit.rshift(perm,1)
+  end
+  return p
+end
+
 function tools.urlEscape(str)
   if type(str) ~= "string" then return error("STR must be a string, provided: "..type(str)) end
   str = str:gsub("\n", "\r\n")
