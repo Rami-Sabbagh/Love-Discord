c7688d15bac16d7096d58e839fc8190e99348775
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-09-30 10:14:44 +0300

    REINFORCEMENT

--- Bot/Events/MESSAGE_CREATE/002_AntiOwO.lua
+++ Bot/Events/MESSAGE_CREATE/002_AntiOwO.lua
@@ -1,8 +1,15 @@
 local discord, data = ...
 
 local chid = data.channel_id
-local content = data.content
+local content = data.content:lower()
 
-if string.find(content:lower(),"owo") or string.find(content:lower(),"uwu") then
-  discord.channels.createMessage(chid, "https://i.redd.it/cqpuzj8avzh11.png")
+local owoPatterns = {
+  "[ou][w]+[ou]",
+  "[ow][u]+[ow]"
+}
+
+for _, pattern in pairs(owoPatterns) do
+  if content:find(pattern) then
+    discord.channels.createMessage(chid, "https://i.redd.it/cqpuzj8avzh11.png")
+  end
 end
