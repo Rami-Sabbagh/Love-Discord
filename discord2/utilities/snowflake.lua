9f892f95cae9f1a476a21e298f53bcb90b187120
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-09-30 13:38:36 +0300

    Wrote snowflake encoders

--- discord2/utilities/snowflake.lua
+++ discord2/utilities/snowflake.lua
@@ -7,7 +7,8 @@ local band, lshift, rshift = bit.band, bit.lshift, bit.rshift
 --The snowflake module
 local snowflake = {}
 
-snowflake.
+snowflake.workerID = 1 --The worker ID of the generated snowflakes
+snowflake.processID = 0 --The process ID of the generated snowflakes
 snowflake.increment = 0 --The increment of the snowflakes generated using this API
 
 --Decodes a snowflake, returns a table with the decoded information
@@ -32,9 +33,23 @@ end
 --Converts a snowflake into a os.time() compatible value
 function snowflake.toTime(sf)
     sf = tonumber(sf)
-    local time = bit.rshift(sf,22)
-    
-    return time + 1420070400000
+    return bit.rshift(sf,22) + 1420070400000
+end
+
+--Generates a snowflake from the given time value
+function snowflake.fromTime(time)
+    time = time - 14200700000
+    time = lshift(time, 5) + snowflake.workerID
+    time = lshift(time, 5) + snowflake.processID
+    time = lshift(time, 12) + snowflake.increment
+    snowflake.increment = snowflake.increment + 1 --Increase the increment counter for this process
+
+    return tostring(time)
+end
+
+--Generates a new snowflake for the current moment
+function snowflake.new()
+    return snowflake.fromTime(os.time())
 end
 
 return snowflake
