cdd78aea952e80a162c3a7df6a85e3676ca091e5
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-04 18:32:00 +0300

    WIP guild object

---
+++ discord2/structures/guild.lua
@@ -0,0 +1,11 @@
+local discord = ... --Passed as an argument.
+local class = discord.class --Middleclass.
+
+local guild = class("discord.structures.Guild")
+
+--New guild object
+function guild:initiliaze()
+    
+end
+
+return guild
