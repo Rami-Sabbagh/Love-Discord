0f0166cd66f9e2ec12dbc85196d36e7a54b93afc
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-05 19:41:12 +0300

    Typofix

--- bot/plugins/basic/init.lua
+++ bot/plugins/basic/init.lua
@@ -9,6 +9,6 @@ plugin.name = "Basic" --The visible name of the plugin
 plugin.version = "V0.0.1" --The visible version string of the plugin
 plugin.description = "Handles basic operations" --The description of the plugin
 plugin.author = "Rami#8688" --Usually the discord tag of the author, but could be anything else
-plugin.authorEmail = "ramilego4game@gmail.com" --The email of the other, could be left empty
+plugin.authorEmail = "ramilego4game@gmail.com" --The email of the auther, could be left empty
 
 return plugin
