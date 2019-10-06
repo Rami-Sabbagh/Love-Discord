5974922e221d7ba98aee6246a95b5353bd07d77f
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-06 17:41:55 +0300

    Made the data storage not write the data tables but only if modified

--- bot/data_storage.lua
+++ bot/data_storage.lua
@@ -14,6 +14,7 @@ local cacheLifeTime = 10 --Keeps the data cached for 10 seconds
 local encodePretty = true --Should the written data be pretty encoded ?
 local cacheTimers = {}
 local internalCache = {}
+local modifiedFlags = {}
 
 --The smart cache table
 local cache = setmetatable({}, {
@@ -30,6 +31,8 @@ local cache = setmetatable({}, {
         local value = {}
         if love.filesystem.getInfo(fileName) then
             value = json:decode(love.filesystem.read(fileName))
+        else
+            modifiedFlags[k] = true --New table
         end
 
         internalCache[k] = value
@@ -44,29 +47,37 @@ local cache = setmetatable({}, {
         cacheTimers[k] = cacheLifeTime --Revive the cached value
         weakCache[k] = v
         internalCache[k] = v
+        modifiedFlags[k] = true
     end,
 
     --Important! for making sure the data is saved and unloaded aftere expiry
-    __call = function(dt)
+    __call = function(t, dt)
         if dt == -1 or dt == -2 then --Force write the data files
             --We write the whole weak referenced data tables
             for k,v in pairs(weakCache) do
-                local data = encodePretty and json:encode_pretty(v) or json:encode(v)
-                love.filesystem.write("/Data/"..key..".json", data)
+                if modifiedFlags[k] then
+                    local data = encodePretty and json:encode_pretty(v) or json:encode(v)
+                    love.filesystem.write("/Data/"..k..".json", data)
+                end
             end
+
+            modifiedFlags = {} --Clear the modified flags
 
             if dt == -2 then --Clear the local cache
                 cacheTimers, internalCache = {}, {} --Poof, all gone
             end
         else --Update the cache timers
             for key, timer in pairs(cacheTimers) do
-                timer = timer - dt
+                cacheTimers[key] = timer - dt
                 if timer <= 0 then --Time is out! Write the data and dereference
-                    local data = encodePretty and json:encode_pretty(internalCache[key]) or json:encode(internalCache[key])
-                    love.filesystem.write("/Data/"..key..".json", data)
+                    if modifiedFlags[key] then
+                        local data = encodePretty and json:encode_pretty(internalCache[key]) or json:encode(internalCache[key])
+                        love.filesystem.write("/Data/"..key..".json", data)
+                    end
 
                     cacheTimers[key] = nil
                     internalCache[key] = nil
+                    modifiedFlags[key] = nil
                     --We keep the weak reference, since that's the point of it
                 end
             end
