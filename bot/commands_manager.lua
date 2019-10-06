5ca6e882acacd928b6100665ea96ee44151df0a3
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-06 15:32:02 +0300

    WIP commands system

--- bot/commands_manager.lua
+++ bot/commands_manager.lua
@@ -9,6 +9,8 @@ function commandsManager:initialize()
     self.botAPI = require("bot")
     self.discord = self.botAPI.discord
 
+    self.defaultPrefix = self.botAPI.config.bot.prefix or "."
+
     --Hook self event
     local hooks = {"MESSAGE_CREATE"}
     for _, hookName in pairs(hooks) do
@@ -38,7 +40,9 @@ end
 function commandsManager:_MESSAGE_CREATE(message)
     local author = message:getAuthor()
     local authorID = author:getID()
+    local channelID = message:getChannelID()
     local content = message:getContent()
+    local guildID = message:getGuildID()
     local replyChannel = message:getReplyChannel() --A channel object for only sending a reply message, it can't be used to tell anything about the channel (except the ID)
 
     --Ignore self messages
@@ -71,7 +75,39 @@ function commandsManager:_MESSAGE_CREATE(message)
         self.botAPI:quit()
     end
 
+    local prefixData = dataStorage["command_manager_prefix"]
 
+    --A list of possible prefixes
+    local prefixes = {}
+    if guildID then
+        local chankey = tostring(guildID).."_"..tostring(channelID)
+        if prefixData[chankey] then
+            prefixes[1] = prefixData[chankey]
+        elseif prefixData[tostring(guildID)] then
+            prefixes[1] = prefixData[tostring(guildID)]
+        else
+            prefixes[1] = self.defaultPrefix
+        end
+    else
+        if prefixData["_"..tostring(channelID)] then
+            prefixes[1] = prefixData["_"..tostring(channelID)]
+        else
+            prefixes[1] = self.defaultPrefix
+        end
+    end
+    prefixes[2] = self.botAPI.me:getTag().." "
+    prefixes[3] = self.botAPI.me:getNickTag().." "
+
+    for id, prefix in ipairs(prefixes) do
+        local prefixLength = #prefix
+        if content:sub(1,prefixLength) == prefix then
+            content = content:sub(prefixLength+1, -1) --Strip the prefix
+        end
+        if id == #prefixes then return end --Didn't match any
+    end
+
+    --Execute the actual command
+    print("COMMAND", content)
 end
 
 return commandsManager
