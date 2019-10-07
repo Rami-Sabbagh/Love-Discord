9a7f92201a88bb4802429b7ea1163ae46cf6fd03
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-07 21:10:00 +0300

    WIP rexcellent games plugin

---
+++ bot/plugins/rexcellent_games/init.lua
@@ -0,0 +1,41 @@
+--Basic operations plugin
+local botAPI, discord, pluginPath, pluginDir = ...
+
+local plugin = {}
+
+local lastMessages = {}
+
+--== Plugin Meta ==--
+
+plugin.name = "Rexcellent Games" --The visible name of the plugin
+plugin.version = "V0.0.1" --The visible version string of the plugin
+plugin.description = "Contains Rexcellent Games Server special commands, only works there" --The description of the plugin
+plugin.author = "Rami#8688" --Usually the discord tag of the author, but could be anything else
+plugin.authorEmail = "ramilego4game@gmail.com" --The email of the auther, could be left empty
+
+--== Commands ==--
+
+plugin.commands = {}; local commands = plugin.commands
+
+function commands.this(message, reply, commandName, ...)
+    local channelID = tostring(message:getChannelID())
+    
+    local lastMessage = lastMessages[channelID]
+    if lastMessage then
+        pcall(lastMessage.addReaction, lastMessage, "this:580812863111954442")
+        lastMessages[channelID] = nil --We don't want to react again
+    end
+
+    pcall(message.delete, message)
+end
+
+--== Events ==--
+
+plugin.events = {}; local events = plugin.events
+
+function events.MESSAGE_CREATE(message)
+    local channelID = tostring(message:getChannelID())
+    lastMessages[channelID] = message
+end
+
+return plugin
