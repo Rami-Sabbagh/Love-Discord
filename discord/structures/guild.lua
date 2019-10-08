586438b41110a62ddc4a898fc4415d5b443003e8
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-08 20:04:33 +0300

    Improve guild object

--- discord/structures/guild.lua
+++ discord/structures/guild.lua
@@ -32,23 +32,50 @@ function guild:initialize(data)
     self.name = data.name --The guild name (string)
     self.ownerID = discord.snowflake(data.owner_id) --The guild owner ID (snowflake)
     self.region = region --TODO: Voice Region structure object
+    self.afkTimeout = data.afk_timeout --Afk timeout in seconds (number)
+    self.verificationLevel = discord.enums.verificationLevels[data.verification_level] --Verification level required for the guild (string)
+    self.defaultMessageNotifications = discord.enums.messageNotificationsLevels[data.default_message_notifications] --Default message notifications level (string)
+    self.explicitContentFilter = discord.enums.explicitContentFilterLevels[data.explicit_content_filter] --Explicit content filter level (string)
+    self.roles = {} --Array of role objects
+    for id, role in pairs(data.roles) do self.roles[id] = discord.role(role) end
+    self.emojis = {} --Array of emoji objects
+    for id, emoji in pairs(data.emojis) do self.emojis[id] = discord.emoji(emoji) end
+    self.features = data.features --Enabled guild features (array of strings)
+    self.mfaLevel = discord.enums.mfaLevels[data.mfa_level] --Required MFA level for the guild (string)
+
 
     --== Optional Fields ==--
     self.icon = data.icon --The guild icon (string - icon hash) --TODO: image url object
     self.splash = data.splash --The guild splash (string - splash hash) --TODO: image url object
     self.owner = data.owner --Whether or not the user is the owner of the guild (boolean/nil), depends on the source of the guild object
-    if data.permissions then
-        self.permissions = discord.permissions(data.permissions) --Total permissions for the user in the guild (does not include channel overrides) (permissions object)
+    --Total permissions for the user in the guild (does not include channel overrides) (permissions object)
+    if data.permissions then self.permissions = discord.permissions(data.permissions) end
+    --Id of afk channel (snowflake)
+    if data.afk_channel_id then self.afkChannelID = discord.snowflake(data.after_channel_id) end
+    self.embedEnabled = data.embed_enabled --Whether this guild is embeddable (e.g. widget) (boolean)
+    if data.embed_channel_id then self.embedChannelID = discord.snowflake(data.embed_channel_id) end --The channel id that the widget will generate an invite to (snowflake)
+    --Application id of the guild creator if it is bot-created (snowflake)
+    if data.application_id then self.applicationID = discord.snowflake(application_id) end
+    self.widgetEnabled = data.widget_enabled --Whether or not the server widget is enabled (boolean)
+    --The channel id for the server widget (snowflake)
+    if data.widget_channel_id then self.widgetChannelID = discord.snowflake(data.widget_channel_id) end
+    --The id of the channel to which system messages are sent (snowflake)
+    if data.system_channel_id then self.systemChannelID = discord.snowflake(data.system_channel_id) end
+    self.joinedAt = data.joined_at --When this guild was joined at (number)
+    self.large = data.large --Whether this is considered a large guild (boolean)
+    self.memberCount = data.member_count --Total number of members in this guild (number)
+    --TODO: voice_states field
+    if data.members then --Users in the guild (array of guild members)
+        self.members = {}
+        for id, member in pairs(data.members) do self.members[id] = discord.guildMember(member) end
     end
-    if self.afk_timeout then
-        --TODO: VOICE CHANNEL OBJECT HERE
+    if data.channels then --Guild channels (array of channels)
+        self.channels = {}
+        for id, channel in pairs(data.channels) do self.channels[id] = discord.channel(channel) end
     end
-    self.embedEnabled = data.embed_enabled
-    if self.embed_channel_id then
-        --TODO: TEXT CHANNEL OBJECT HERE
-    end
+    --TODO: Presences field
 
-    --TODO: Add the fields after the verification_level one https://discordapp.com/developers/docs/resources/guild
+    --TODO: Add the fields after the presences one https://discordapp.com/developers/docs/resources/guild
 
 end
 
