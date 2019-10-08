f7196b042d49a5d7119fbe6076a2442fa97d2ea0
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-09 00:18:16 +0300

    Huge overall on the basic commands

--- bot/plugins/basic/init.lua
+++ bot/plugins/basic/init.lua
@@ -17,21 +17,35 @@ plugin.authorEmail = "ramilego4game@gmail.com" --The email of the auther, could 
 
 --== Commands ==--
 
+--Shared embed, could be used by any command
+local adminEmbed = discord.embed()
+adminEmbed:setTitle("You need to have administrator permissions to use this command :warning:")
+
+--Shared embed, could be used by any command
+local developerEmbed = discord.embed()
+developerEmbed:setTitle("This command could be only used by the bot's developers :warning:")
+
 plugin.commands = {}; local commands = plugin.commands
 
-function commands.commands(message, reply, commandName, ...)
-    local commandsList = {}
-    for c in pairs(commandsManager:getCommands()) do
-        commandsList[#commandsList + 1] = c
-    end
-    commandsList = table.concat(commandsList, ", ")
+--Commands command, lists available commands
+do
+    local commandsEmbed = discord.embed()
+    commandsEmbed:setTitle("Available commands: :tools:")
 
-    reply:send(table.concat({
-        "**Available commands** :tools:",
-        "```css",
-        commandsList,
-        "```"
-    },"\n"))
+    function commands.commands(message, reply, commandName, ...)
+        if not commandsEmbed:getDescription() then
+            local commandsList = {}
+            for c in pairs(commandsManager:getCommands()) do commandsList[#commandsList + 1] = c end
+            commandsList = table.concat(commandsList, ", ")
+            commandsEmbed:setDescription(table.concat({
+                "```css",
+                commandsList,
+                "```"
+            }, "\n"))
+        end
+
+        reply:send(false, commandsEmbed)
+    end
 end
 
 function commands.ping(message, reply, commandName, ...)
@@ -43,40 +57,56 @@ function commands.ping(message, reply, commandName, ...)
     reply:send(pong.." :ping_pong:"..explosion)
 end
 
-function commands.reload(message, reply, commandName, ...)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
+--Reload command
+do
+    local reloadEmbedSuccess = discord.embed()
+    reloadEmbedSuccess:setTitle("Reloaded successfully :white_check_mark:")
+    local reloadEmbedFailure = discord.embed()
+    reloadEmbedFailure:setTitle("Failed to reload :warning:")
 
-    local ok, err = pluginManager:reload()
-    if ok then
-        commandsManager:reloadCommands()
-        reply:send("Reloaded successfully :white_check_mark:")
-    else
-        reply:send("Failed to reload :warning:\n||```\n"..err.."\n```||")
+    function commands.reload(message, reply, commandName, ...)
+        if not botAPI:isFromDeveloper(message) then reply:send(false, developerEmbed) return end
+
+        local ok, err = pluginManager:reload()
+        if ok then
+            commandsManager:reloadCommands()
+            reply:send(false, reloadEmbedSuccess)
+        else
+            reloadEmbedFailure:setDescription("||```\n"..err:gsub("plugin: ","plugin:\n").."\n```||")
+            reply:send(false, reloadEmbedFailure)
+        end
     end
 end
 
 function commands.stop(message, reply, commandName, ...)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
+    if not botAPI:isFromDeveloper(message) then reply:send(false, developerEmbed) return end
     reply:send("Goodbye :wave:")
     love.event.quit()
 end
 
-function commands.restart(message, reply, commandName, ...)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
-    
-    love.event.quit("restart")
+--Restart command
+do
+    local restartEmbed = discord.embed()
+    restartEmbed:setTitle(":gear: Restarting :gear:")
+    restartEmbed:setDescription("This might take a while...")
+    function commands.restart(message, reply, commandName, ...)
+        if not botAPI:isFromDeveloper(message) then reply:send(false, developerEmbed) return end
+        
+        love.event.quit("restart")
 
-    local pdata = dataStorage["plugins/basic/restart"]
-    pdata.channelID = tostring(message:getChannelID())
-    dataStorage["plugins/basic/restart"] = pdata
+        local pdata = dataStorage["plugins/basic/restart"]
+        pdata.channelID = tostring(message:getChannelID())
+        pdata.timestamp = os.time()
+        dataStorage["plugins/basic/restart"] = pdata
 
-    reply:send("Restarting :gear: :gear: :gear:")
-    reply:triggerTypingIndicator()
-    discord.gateway.disconnect = function() end --Show the bot as online while restarting xd
+        reply:send(false, restartEmbed)
+        reply:triggerTypingIndicator()
+        discord.gateway.disconnect = function() end --Show the bot as online while restarting xd
+    end
 end
 
 function commands.dumpdata(message, reply, commandName, dname)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
+    if not botAPI:isFromDeveloper(message) then reply:send(false, developerEmbed) return end
     if not dname then reply:send("Missing package name!") end
 
     local data = discord.json:encode_pretty(dataStorage[dname])
@@ -94,32 +124,77 @@ function commands.dumpdata(message, reply, commandName, dname)
 end
 
 function commands.data(message, reply, commandName, action, dname)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
+    if not botAPI:isFromDeveloper(message) then reply:send(false, developerEmbed) return end
 end
 
-function commands.setprefix(message, reply, commandName, level, newPrefix)
-    if not botAPI:isFromDeveloper(message) then reply:send("This command is for developers only :warning:") return end
+--Setprefix command
+do
+    local setprefixHelpDM = discord.embed()
+    setprefixHelpDM:setTitle("Usage: :notepad_spiral:")
+    setprefixHelpDM:setDescription(table.concat({
+        "```css",
+        "setprefix channel <new_prefix>",
+        "setprefix clear channel",
+        "```"
+    },"\n"))
 
-    local guildID = message:getGuildID()
+    local setprefixHelp = discord.embed(setprefixHelpDM:getAll())
+    setprefixHelp:setDescription(table.concat({
+        "```css",
+        "setprefix channel <new_prefix>",
+        "setprefix guild <new_prefix>",
+        "setprefix clear channel",
+        "setprefix clear guild",
+        "```"
+    },"\n"))
 
-    if not (level and newPrefix) or (level ~= "guild" and level ~= "channel") or (level == "guild" and not guildID) then
-        reply:send(table.concat({
-            "**Usage: :notepad_spiral: **",
-            "```css",
-            "setprefix channel <new_prefix>",
-            guildID and "setprefix guild <new_prefix>" or "",
-            "```"
-        },"\n"))
-        return
+    local replyEmbed = discord.embed()
+
+    function commands.setprefix(message, reply, commandName, level, newPrefix)
+        if not botAPI:isFromAdmin(message) then reply:send(false, adminEmbed) return end
+
+        local guildID = message:getGuildID()
+
+        local prefixData = dataStorage["commands_manager/prefix"]
+
+        if guildID then
+            local guildPrefix = prefixData[tostring(guildID)]
+            local channelPrefix = prefixData[tostring(guildID or "").."_"..tostring(message:getChannelID())]
+            setprefixHelp:setField(1, "Guild's Prefix:", guildPrefix and "`"..guildPrefix.."`" or "default (`"..commandsManager.defaultPrefix.."`)", true)
+            setprefixHelp:setField(2, "Channel's Prefix:", channelPrefix and "`"..channelPrefix.."`" or "not set", true)
+        else
+            local channelPrefix = prefixData["_"..tostring(message:getChannelID())]
+            setprefixHelpDM:setField(1, "DM Channel's Prefix:", channelPrefix and "`"..channelPrefix.."`" or "default (no prefix)")
+        end
+
+        if not (level and newPrefix) then reply:send(false, guildID and setprefixHelp or setprefixHelpDM) return end
+
+        local prefixType
+
+        if level == "clear" then
+            if newPrefix == "guild" and not guildID or (newPrefix ~= "guild" and newPrefix ~= "channel") then
+                reply:send(false, guildID and setprefixHelp or setprefixHelpDM) return end
+            prefixType = newPrefix
+        else
+            if level == "guild" and not guildID or (level ~= "guild" and level ~= "channel") then
+                reply:send(false, guildID and setprefixHelp or setprefixHelpDM) return end
+            prefixType = level
+        end
+
+        local prefixKey = (prefixType == "guild") and tostring(guildID) or tostring(guildID or "").."_"..tostring(message:getChannelID())
+
+        if level == "clear" then
+            prefixData[prefixKey] = nil
+            replyEmbed:setTitle("The "..newPrefix.."'s commands prefix has been cleared successfully :white_check_mark:")
+            reply:send(false, replyEmbed)
+        else
+            prefixData[prefixKey] = newPrefix
+            replyEmbed:setTitle(level:sub(1,1):upper()..level:sub(2,-1).."'s commands prefix has been set to `"..newPrefix.."` successfully :white_check_mark:")
+            reply:send(false, replyEmbed)
+        end
+
+        dataStorage["commands_manager/prefix"] = prefixData
     end
-
-    local prefixKey = (level == "guild") and tostring(guildID) or tostring(guildID or "").."_"..tostring(message:getChannelID())
-
-    local prefixData = dataStorage["commands_manager/prefix"]
-    prefixData[prefixKey] = newPrefix
-    dataStorage["commands_manager/prefix"] = prefixData
-
-    reply:send(level:sub(1,1):upper()..level:sub(2,-1).."'s commands prefix has been set to `"..newPrefix.."` successfully :white_check_mark:")
 end
 
 --[[
@@ -132,17 +207,27 @@ end
 
 plugin.events = {}; local events = plugin.events
 
-function events.READY(data)
-    local pdata = dataStorage["plugins/basic/restart"]
-    if pdata.channelID then
-        local replyChannel = discord.channel{
-            id = pdata.channelID,
-            type = discord.enums.channelTypes["GUILD_TEXT"]
-        }
-        pdata.channelID = nil
-        dataStorage["plugins/basic/restart"] = pdata
+do
+    local restartedEmbed = discord.embed()
+    restartedEmbed:setTitle("Restarted Successfully :white_check_mark:")
 
-        pcall(replyChannel.send, replyChannel, "Restarted Successfully :white_check_mark:")
+    function events.READY(data)
+        local pdata = dataStorage["plugins/basic/restart"]
+        if pdata.channelID then
+            local replyChannel = discord.channel{
+                id = pdata.channelID,
+                type = discord.enums.channelTypes["GUILD_TEXT"]
+            }
+
+            local delay = os.time() - pdata.timestamp
+            restartedEmbed:setDescription("Operation took "..delay.." seconds:stopwatch:")
+
+            pdata.channelID = nil
+            pdata.timestamp = nil
+            dataStorage["plugins/basic/restart"] = pdata
+
+            pcall(replyChannel.send, replyChannel, false, restartedEmbed)
+        end
     end
 end
 
