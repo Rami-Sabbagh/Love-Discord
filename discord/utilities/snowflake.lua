215816462b55f01a1a676ebc990d3f35275171ba
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-09 18:46:12 +0300

    Fix snowflake timestamp decoding, using some math _tricks_

--- discord/utilities/snowflake.lua
+++ discord/utilities/snowflake.lua
@@ -3,6 +3,7 @@ local bit = discord.utilities.bit --Universal bitwise interface
 
 --Localised functions and values
 local band, lshift, rshift = bit.band, bit.lshift, bit.rshift
+local floor = math.floor
 
 --The snowflake module
 local snowflake = {}
@@ -11,11 +12,46 @@ snowflake.workerID = 1 --The worker ID of the generated snowflakes
 snowflake.processID = 0 --The process ID of the generated snowflakes
 snowflake.increment = 0 --The increment of the snowflakes generated using this API
 
+--Divides a decimal number string by 2, the result is also a string
+local function divideByTwo(str)
+    local remainder = 0
+    local newNum, newNumLen = {}, 1
+    for digit in str:gmatch("%d") do
+        newNum[newNumLen] = floor(digit/2) + remainder
+        remainder = digit % 2 > 0 and 5 or 0
+        newNumLen = newNumLen + 1
+    end
+    return table.concat(newNum, ""):gsub("^0*", ""), (tonumber(str:sub(-1, -1)) % 2 == 1)
+end
+
+--Converts a decimal string into a binary string, using manual math
+local function str2bin(str)
+    local bits, bitslen = {}, 1
+    local isOne
+    while str ~= "" do
+        str, isOne = divideByTwo(str)
+        bits[bitslen] = isOne and "1" or "0"
+        bitslen = bitslen + 1
+    end
+    if bitslen % 2 == 0 then bits[bitslen] = "" else bitslen = bitslen - 1 end
+    --Reverse bits order
+    for i=1, bitslen/2 do bits[i], bits[bitslen-i+1] = bits[bitslen-i+1], bits[i] end
+    bits = table.concat(bits, "")
+    return bits
+end
+
+--For fixing the decoded timestamp
+local fixFactor = 1024/1000
+
 --Decodes a snowflake, returns a table with the decoded information
 function snowflake.decode(sf)
+    local timeBits = str2bin(sf)
+    local largePart, smallPart = timeBits:sub(1,-33), timeBits:sub(-32, -23)
+    largePart, smallPart = tonumber(largePart, 2) or 0, tonumber(smallPart, 2) or 0
+    local time = largePart*fixFactor + smallPart/1000
+    time = math.floor(time + 1420070400)
+    
     sf = tonumber(sf)
-    local time = bit.rshift(sf,22)
-    time = time + 1420070400
 
     local workerID = bit.band(sf,0x3E0000)
     workerID = bit.rshift(workerID,17)
