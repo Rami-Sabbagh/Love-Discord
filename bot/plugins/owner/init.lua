96efcbea8d9d59aeae0bec545dd11719ecb0a80a
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-16 23:32:55 +0300

    Wrote owner commands help

--- bot/plugins/owner/init.lua
+++ bot/plugins/owner/init.lua
@@ -11,8 +11,8 @@ local plugin = {}
 --== Plugin Meta ==--
 
 plugin.name = "Owner" --The visible name of the plugin
-plugin.icon = ":question:" --The plugin icon to be shown in the help command
-plugin.version = "V1.0.0" --The visible version string of the plugin
+plugin.icon = ":radio_button:" --The plugin icon to be shown in the help command
+plugin.version = "V2.0.0" --The visible version string of the plugin
 plugin.description = "Contains commands available only to the owners of the bot." --The description of the plugin
 plugin.author = "Rami#8688" --Usually the discord tag of the author, but could be anything else
 plugin.authorEmail = "ramilego4game@gmail.com" --The email of the auther, could be left empty
@@ -27,12 +27,18 @@ plugin.commands = {}; local commands = plugin.commands
 
 --Reload command
 do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("reload")
+    usageEmbed:setDescription("Reloads the bot plugins.")
+    usageEmbed:setField(1, "Usage: :notepad_sprial:", "```css\nreload\n```")
+
     local reloadEmbedSuccess = discord.embed()
     reloadEmbedSuccess:setTitle("Reloaded successfully :white_check_mark:")
     local reloadEmbedFailure = discord.embed()
     reloadEmbedFailure:setTitle("Failed to reload :warning:")
 
     function commands.reload(message, reply, commandName, ...)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
         if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
 
         local ok, err = pluginsManager:reload()
@@ -46,18 +52,33 @@ do
     end
 end
 
-function commands.stop(message, reply, commandName, ...)
-    if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
-    reply:send("Goodbye :wave:")
-    love.event.quit()
+--Stop command
+do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("stop")
+    usageEmbed:setDescription("Terminates the bot's process.")
+    usageEmbed:setField(1, "Usage: :notepad_sprial:", "```css\nstop\n```")
+
+    function commands.stop(message, reply, commandName, ...)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
+        if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
+        reply:send("Goodbye :wave:")
+        love.event.quit()
+    end
 end
 
 --Restart command
 do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("restart")
+    usageEmbed:setDescription("Restarts the bot's process.")
+    usageEmbed:setField(1, "Usage: :notepad_spiral:", "```css\nrestart\n```")
+
     local restartEmbed = discord.embed()
     restartEmbed:setTitle(":gear: Restarting :gear:")
     restartEmbed:setDescription("This might take a while...")
     function commands.restart(message, reply, commandName, ...)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
         if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
         
         love.event.quit("restart")
@@ -71,33 +92,39 @@ do
     end
 end
 
-function commands.dumpdata(message, reply, commandName, dname)
-    if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
-    if not dname then reply:send("Missing package name!") end
+--Dumpdata command
+do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("dumpdata")
+    usageEmbed:setDescription("Dumps the storage data for the requested package.")
+    usageEmbed:setField(1, "Usage: :notepad_spiral:", "```css\ndumpdata <package_name>\n```")
 
-    local data = discord.json:encode_pretty(dataStorage[dname])
-    local message = table.concat({
-        "```json",
-        data,
-        "```"
-    },"\n")
-    
-    if #message > 2000 then
-        reply:send("Data too large, uploaded in a file :wink:", false, {dname:gsub("/","_")..".json",data})
-    else
-        reply:send(message)
+    function commands.dumpdata(message, reply, commandName, dname)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
+        if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
+        if not dname then reply:send(false, usageEmbed) return end end
+
+        local data = discord.json:encode_pretty(dataStorage[dname])
+        local message = table.concat({
+            "```json",
+            data,
+            "```"
+        },"\n")
+        
+        if #message > 2000 then
+            reply:send("Data too large, uploaded in a file :wink:", false, {dname:gsub("/","_")..".json",data})
+        else
+            reply:send(message)
+        end
     end
-end
-
-function commands.data(message, reply, commandName, action, dname)
-    if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
 end
 
 --Execute command
 do
     local executeUsage = discord.embed()
-    executeUsage:setTitle("Usage: :notepad_spiral:")
-    executeUsage:setDescription(table.concat({
+    executeUsage:setTitle("execute")
+    executeUsage:setDescription("Executes Lua code under the bot's environment.")
+    executeUsage:setField(1, "Usage: :notepad_spiral:", table.concat({
         "```css",
         "execute <lua_code_block> [no_log]",
         "```"
@@ -110,6 +137,7 @@ do
     outputEmbed:setTitle("Executed successfully :white_check_mark:")
 
     function commands.execute(message, reply, commandName, luaCode, nolog, ...)
+        if commandName == "?" then reply:send(false, executeUsage) return end --Triggered using the help command
         if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
         if not luaCode then reply:send(false, executeUsage) return end
 
@@ -166,6 +194,11 @@ end
 
 --Pulls the bot's git repository
 do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("gitupdate")
+    usageEmbed:setDescription("Updates the bot's internal code from the git repository.")
+    usageEmbed:setField(1, "Usage: :notepad_spiral:", "```css\ngitupdate\n```")
+
     --Executes a command, and returns it's output
     local function capture(cmd, raw)
         local f = assert(io.popen(cmd, 'r'))
@@ -179,6 +212,7 @@ do
     end
 
     function commands.gitupdate(message, reply, commandName, ...)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
         if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
         local output1 = capture("git -C "..love.filesystem.getSource().." add *")
         local output2 = capture("git -C "..love.filesystem.getSource().." checkout -- "..love.filesystem.getSource())
@@ -196,6 +230,11 @@ end
 
 --CMD command
 do
+    local usageEmbed = discord.embed()
+    usageEmbed:setTitle("cmd")
+    usageEmbed:setDescription("Executes a shell command under the bot's host system.")
+    usageEmbed:setField(1, "Usage: :notepad_sprial:", "```css\ncmd <command> [...]\n```")
+
     --Executes a command, and returns it's output
     local function capture(cmd, raw)
         local f = assert(io.popen(cmd, 'r'))
@@ -212,6 +251,7 @@ do
     resultEmbed:setTitle("Execution output: :scroll:")
 
     function commands.cmd(message, reply, commandName, ...)
+        if commandName == "?" then reply:send(false, usageEmbed) return end --Triggered using the help command
         if not botAPI:isFromOwner(message) then reply:send(false, ownerEmbed) return end
         local cmd = table.concat({...}, " ")
         local output = capture(cmd)
