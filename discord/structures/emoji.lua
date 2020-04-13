63d1800002ebf2ecaf40e6e4dbb0493451c9dd2c
Rami Sabbagh <ramilego4game@gmail.com>
2020-04-13 13:28:24 +0300

    Document the emoji class

--- discord/structures/emoji.lua
+++ discord/structures/emoji.lua
@@ -1,4 +1,8 @@
---- Emoji class
+--- The discord emoji class.
+-- It can be obtained either from a discord event, from some discörd objects, by constructing one for a standard emoji
+-- or by fetching the custom emoji by it's id and guild id from discord servers.
+-- @usage local standardEmoji = discord.emoji("smiley") --The constructed standard emoji.
+-- @usage local customEmoji = discord.emoji("guild_id", "emoji_id") --The requested custom emoji.
 -- @classmod discord.emoji
 
 local discord = ... --Passed as an argument.
@@ -17,7 +21,11 @@ end
 
 --TODO: Add weak tables caching of custom emojis
 
---New guild member object
+--- Create a new emoji object.
+-- @tparam string|table data Either the emoji name string for a standard emoji, the standard emoji unicode string,
+-- the guild ID string for a custom emoji or an emoji data table (from an internal api).
+-- @tparam ?string emojiID The emoji ID string for a custom emoji.
+-- @raise Request error when fails to request the custom emoji from discord servers.
 function emoji:initialize(data, emojiID)
     if type(data) == "string" then
         --Fetch custom emoji
@@ -42,24 +50,41 @@ function emoji:initialize(data, emojiID)
 
     --== Basic Fields ==--
 
-    self.name = data.name --Emoji name (string)
+    --- Internal fields.
+    -- @section internal_fields
+
+    --- Emoji name (string).
+    self.name = data.name
 
     --== Optional Fields ==--
 
-    --Emoji ID (snowflake)
+    --- Internal optional fields.
+    -- @section internal_optional_fields
+
+    --- Emoji ID (snowflake).
+    -- @field self.id
     if data.id then self.id = discord.snowflake(data.id) end
-    --Roles this emoji is whitelisted to (array of role objects)
+    --- Roles this emoji is whitelisted to (array of role objects).
+    -- @field self.roles
     if data.roles then
         self.roles = {}
         for id, snowflake in pairs(data.roles) do
             self.roles[id] = discord.snowflake(snowflake)
         end
     end
-    --User that created this emoji (user)
+    --- User that created this emoji (user).
+    -- @field self.user
     if data.user then self.user = discord.user(data.user) end
-    self.requireColons = data.require_colons --Whether this emoji must be wrapped in colons (boolean)
-    self.managed = data.managed --Whether this emoji is managed (boolean)
-    self.animated = data.animated --Whether this emoji is animated (boolean)
+
+    --- Whether this emoji must be wrapped in colons (boolean).
+    self.requireColons = data.require_colons
+    --- Whether this emoji is managed (boolean).
+    self.managed = data.managed
+    --- Whether this emoji is animated (boolean).
+    self.animated = data.animated
+
+    ---
+    -- @section end
 
     --== Fix standard (unicode) emojis ==--
     if not self.id then
@@ -71,15 +96,23 @@ end
 
 --== Methods ==--
 
---Returns emoji ID (snowflake/nil)
+--- Get the emoji ID.
+-- @treturn ?discord.snowflake The emoji's ID, `nil` when not known.
 function emoji:getID() return self.id end
 
---Returns emoji name (string)
+--- Get the emoji name.
+-- @treturn string The emoji's name.
 function emoji:getName() return self.name end
 
 --== Operators Overrides ==--
 
---Format the emoji into it's message tag
+--- Operators Overrides.
+-- @section operators_overrides
+
+--- Format the emoji into it's message tag.
+-- Handles standard emojis custom emojis, and custom animated emojis properly.
+-- @treturn string The emoji's message tag, ex: `<:emoji_name:emoji_id>`.
+-- @usage local emoji_tag = tostring(my_emoji)
 function emoji:__tostring()
     if not self.id then
         return discord.utilities.message.emojis[self.name]
